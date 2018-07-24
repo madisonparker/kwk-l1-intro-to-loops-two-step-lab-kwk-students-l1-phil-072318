137ef2961d624fd0ae83d738ee2f0e8b73e4579a
madisonparker <christaandmadison@gmail.com>
2018-07-24 19:57:35 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -27,7 +27,9 @@ def a_few_more_steps
 end
 
 def how_many_steps?
-  # Write a loop that fulfills all criteria from Part 3 of the README.md
+  puts "Turn"
+  sleep (0.5)
+  puts ""
 end
 
 def break_dance
